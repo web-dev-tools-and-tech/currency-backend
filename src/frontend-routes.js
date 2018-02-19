0af6fef3101087c65f769c0946c88e4b7c052b6f
Gil Tayar <gil@tayar.org>
2018-02-19 21:41:12 +0200

    now frontend is using currency-backend for rates

--- src/frontend-routes.js
+++ src/frontend-routes.js
@@ -9,6 +9,13 @@ function frontendRoutes(app, frontendAdress, onlyIfLoggedIn) {
       proxyReqPathResolver(req) {
         return '/'
       },
+      userResDecorator(proxyRes, proxyResData, userReq, userRes) {
+        let html = proxyResData.toString('utf8')
+
+        html = '<script>window.useCurrencyBackend = true</script>' + html
+
+        return html
+      },
     }),
   )
   app.get(/\./, onlyIfLoggedIn, proxy(frontendAdress))
